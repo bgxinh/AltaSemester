c8efa243688fe31b7d13882f996208259fa0cc7d
bgxinh <nguyenduyduc0609@gmail.com>
2024-12-04 14:49:33 +0700

    Update UserActivityHub.cs

--- AltaSemester/Hubs/UserActivityHub.cs
+++ AltaSemester/Hubs/UserActivityHub.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System.Runtime.CompilerServices;
+
 namespace AltaSemester.Hubs
 {
-    public class UserActitvityHub : Hub
+    public class UserActivityHub : Hub
     {
-        public async Task BroadscastMessage (string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage",message);
-        }
+        public async Task BroadcastMessage(string message)
+            => await Clients.All.SendAsync("ReceiveMessage", message);
+
+        public async Task SendMessageLogin(string username)
+            => await Clients.All.SendAsync("UserLogin", username);
+
+        public async Task SendMessageLogout(string username) 
+            => await Clients.All.SendAsync("UserLogout", username);
     }
 }
+
