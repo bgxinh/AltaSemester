02da613f216c946f1aa8f23ab2571effe0f6b6c7
bgxinh <nguyenduyduc0609@gmail.com>
2024-12-05 21:48:21 +0700

    commit _manager

--- AltaSemester.Service/Cores/DeviceService.cs
+++ AltaSemester.Service/Cores/DeviceService.cs
@@ -1,4 +1,5 @@
-﻿using AltaSemester.Service.Cores.Interface;
+﻿using AltaSemester.Data.Dtos;
+using AltaSemester.Service.Cores.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,29 @@ namespace AltaSemester.Service.Cores
 {
     public class DeviceService : IDevice
     {
+        public Task<ModelResult> AddNewDevice(DeviceDto deviceDto)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<ModelResult> DeleteDevice(string deviceCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ModelResult> EditDevice(DeviceDto deviceDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ModelResult> GetAllDevice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ModelResult> GetDevicePage(int pageNumber, int pageSize, bool? Status, bool? StatusConnect)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- AltaSemester.Service/Cores/Interface/IDevice.cs
+++ AltaSemester.Service/Cores/Interface/IDevice.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AltaSemester.Data.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace AltaSemester.Service.Cores.Interface
 {
     public interface IDevice
     {
-
+        public Task<ModelResult> GetAllDevice();
+        public Task<ModelResult> GetDevicePage(int pageNumber, int pageSize, bool? Status, bool? StatusConnect);
+        public Task<ModelResult> EditDevice(DeviceDto deviceDto);
+        public Task<ModelResult> DeleteDevice(string deviceCode);
+        public Task<ModelResult> AddNewDevice(DeviceDto deviceDto);
     }
 }
